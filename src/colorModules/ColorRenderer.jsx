3db3d4db4d6428728cd2ec3a72fd600dbe27d3ac
Johan Swan <jswannieman@gmail.com>
2023-12-29 11:58:46 +1300

    Update ColorRenderer JSX Component

--- src/colorModules/ColorRenderer.jsx
+++ src/colorModules/ColorRenderer.jsx
@@ -28,20 +28,8 @@ const colors = [
 ];
 
 export default function ColorRenderer() {
-  const changeColor1 = () => {
-    document.querySelector(".color-block").style.color = "#000";
-  };
-
-  const changeColor2 = () => {
-    document.querySelector(".color-block").style.color = "#FFF";
-  };
-
   return (
-    <div
-      className="color-block"
-      onMouseEnter={changeColor1}
-      onMouseLeave={changeColor2}
-    >
+    <div className="color-block">
       {colors.map((color) => (
         <Color
           key={color.hex}
