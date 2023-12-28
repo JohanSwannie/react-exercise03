03084e12360e5b66e0741135caa219098cbd227f
Johan Swan <jswannieman@gmail.com>
2023-12-29 12:03:44 +1300

    Update Color JSX Component

--- src/colorModules/Color.jsx
+++ src/colorModules/Color.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 const Color = ({ hex, description }) => {
+  const changeColor1 = () => {
+    document.getElementById(`${description}`).style.color = "black";
+  };
+  const changeColor2 = () => {
+    document.getElementById(`${description}`).style.color = "white";
+  };
   return (
-    <div style={{ backgroundColor: hex }}>
-      <h2>{description}</h2>
+    <div
+      style={{ backgroundColor: hex }}
+      onMouseEnter={changeColor1}
+      onMouseLeave={changeColor2}
+    >
+      <h2 id={description}>{description}</h2>
     </div>
   );
 };
