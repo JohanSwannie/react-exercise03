b5b3465ee4f6bf4df4329dfc91ea57a5c7e9964b
Johan Swan <jswannieman@gmail.com>
2023-12-29 16:27:23 +1300

    Update Color JSX Component

--- src/colorModules/Color.jsx
+++ src/colorModules/Color.jsx
@@ -3,14 +3,10 @@ import React from "react";
 const Color = ({ hex, description }) => {
   const changeColor1 = () => {
     document.getElementById(`${description}`).style.color = "black";
-    document.getElementById(`${description}`).style.padding = ".75rem";
-    document.getElementById(`${description}`).style.border = "2px solid white";
     document.body.style.backgroundColor = hex;
   };
   const changeColor2 = () => {
     document.getElementById(`${description}`).style.color = "white";
-    document.getElementById(`${description}`).style.padding = "0";
-    document.getElementById(`${description}`).style.border = "none";
     document.body.style.backgroundColor = "black";
   };
   return (
